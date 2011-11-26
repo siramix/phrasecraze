fa99e4d5ca4bde57f5eb369c9d1899111100eeeb
LucasRowe <lucas.rowe@gmail.com>
2011-11-25 21:04:57 -0500

    Hammering out front end issues with phrase pack purchase activity.

--- src/com/siramix/phrasecraze/PhrasePackPurchase.java
+++ src/com/siramix/phrasecraze/PhrasePackPurchase.java
@@ -13,6 +13,7 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.ScrollView;
+import android.widget.TextView;
 
 public class PhrasePackPurchase extends Activity {
 
@@ -37,37 +38,55 @@ public class PhrasePackPurchase extends Activity {
     setVolumeControlStream(AudioManager.STREAM_MUSIC);
 
     // Setup the view
-    this.setContentView(R.layout.turnsummary);
+    this.setContentView(R.layout.packpurchase);
+    
+    // Get our current context
     PhraseCrazeApplication application = (PhraseCrazeApplication) this
         .getApplication();
     GameManager game = application.getGameManager();
-/*
+
     // Populate and display list of cards
-    ScrollView list = (ScrollView) findViewById(R.id.PackPurchase_PackList);
+    LinearLayout packSets = (LinearLayout) findViewById(R.id.PackPurchase_PackSets);
     LinearLayout layout = new LinearLayout(this.getBaseContext());
     layout.setOrientation(LinearLayout.VERTICAL);
 
     // Iterate through all completed cards and set layout accordingly
     mPackViewList = new LinkedList<ImageView>();
     mPackLineList = new LinkedList<View>();
+    mPackList = new LinkedList<String>();
     //TODO get this list from the database or marketplace if possible 
     mPackList.add("Test1");
     mPackList.add("Test2");
+    mPackList.add("Test3");
     
     String packname;
     int count = 0;
 
     for (Iterator<String> it = mPackList.iterator(); it.hasNext();) {
       packname = it.next();
-
+      
       LinearLayout line = (LinearLayout) LinearLayout.inflate(
           this.getBaseContext(), R.layout.packpurchaserow, layout);
-      RelativeLayout realLine = (RelativeLayout) line.getChildAt(count);
+      RelativeLayout packRow = (RelativeLayout) line.getChildAt(count);
+
       // Make every line alternating color
       if (count % 2 == 0) {
-        View background = (View) realLine.getChildAt(0);
+        View background = (View) packRow.getChildAt(0);
         background.setBackgroundResource(R.color.genericBG_trim);
       }
+      
+      // Set Pack Title
+      TextView packTitle = (TextView) packRow.getChildAt(1);
+      //TODO this will need to pull the string from our pack list
+      packTitle.setText(it.next());
+
+      // Set Row end icon
+      ImageView packIcon = (ImageView) packRow.getChildAt(2);
+      packIcon.setImageResource(R.drawable.logoram);
+      mPackViewList.add(packIcon);
+      mPackLineList.add(packRow);
+      count++;
     }
-  */}
+    packSets.addView(layout);
+  }
 }
