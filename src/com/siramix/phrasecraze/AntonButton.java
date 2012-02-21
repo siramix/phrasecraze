d5dd739920134c306d84e5d148581265100a1af0
Edward Rowe <tehgiver@gmail.com>
2012-02-21 00:23:15 -0500

    Added AntonButton for Anton font on buttons (duh?).
    Converted all views with generic buttons to use the
    custom AntonButton.

---
+++ src/com/siramix/phrasecraze/AntonButton.java
@@ -0,0 +1,37 @@
+package com.siramix.phrasecraze;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.Button;
+
+public class AntonButton extends Button {
+
+  public AntonButton(Context context) {
+    super(context);
+    setCustomFont();
+  }
+
+  public AntonButton(Context context, AttributeSet attrs) {
+    super(context, attrs);
+    setCustomFont();
+  }
+
+  public AntonButton(Context context, AttributeSet attrs, int defStyle) {
+    super(context, attrs, defStyle);
+    setCustomFont();
+  }
+
+  /*
+   * Helper function to set the custom typeface of this view.
+   */
+  private void setCustomFont()
+  {
+    // Wrap in isEditMode so that xml previwer doesn't break.
+    if (!this.isInEditMode()) {
+      Typeface typeface = Typeface.createFromAsset(this.getContext()
+          .getAssets(), "fonts/Anton.ttf");
+      setTypeface(typeface);
+    }
+  }
+}
